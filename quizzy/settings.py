af61de92ca6541163c6aa13e8e860dd768f56005
Rodrigue Roland CODO <codorol22@gmail.com>
2022-12-06 20:24:58 +0500

    -

--- quizzy/settings.py
+++ quizzy/settings.py
@@ -4,7 +4,6 @@ from pathlib import Path
 
 import dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 dotenv.read_dotenv(BASE_DIR / '.env')
