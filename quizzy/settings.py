89f5c3fc648a2e733537bbdc3e421ef5aada555d
Rodrigue Roland CODO <codorol22@gmail.com>
2022-12-07 01:05:43 +0500

    -

--- quizzy/settings.py
+++ quizzy/settings.py
@@ -117,9 +117,6 @@ STATICFILES_DIRS = [
     BASE_DIR / 'staticfiles'
 ]
 
-STATICFILES_DIRS = BASE_DIR / 'static'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
