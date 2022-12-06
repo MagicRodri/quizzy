25c39fe99e507f7eb9115be245cc8f52b6592e1a
Rodrigue Roland CODO <codorol22@gmail.com>
2022-12-06 20:43:48 +0500

    -

--- quizzy/settings.py
+++ quizzy/settings.py
@@ -64,7 +64,7 @@ WSGI_APPLICATION = 'quizzy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'quizzy',
+        'NAME': os.getenv('DATABASE_NAME'),
         'USER' : os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST' : os.getenv('DATABASE_HOST'),
